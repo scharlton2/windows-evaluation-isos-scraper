ae96cb0cdfa1ec171c0d183131ae23f48b4ce25c
Charlton, Scott R. <charlton@usgs.gov>
2025-01-07 17:35:45 -0700

    Added Win10 and Win2019

--- scrape.js
+++ scrape.js
@@ -71,7 +71,9 @@ async function main(name) {
         );
 
         var targets = {
+            "windows-10":   [null,    "https://www.microsoft.com/en-us/evalcenter/download-windows-10-enterprise"],
             "windows-11":   [null,    "https://www.microsoft.com/en-us/evalcenter/download-windows-11-enterprise"],
+            "windows-2019": [null,    "https://www.microsoft.com/en-us/evalcenter/evaluate-windows-server-2019"],
             "windows-2022": [null,    "https://www.microsoft.com/en-us/evalcenter/download-windows-server-2022"],
             "windows-2025": [null,    "https://www.microsoft.com/en-us/evalcenter/download-windows-server-2025"],
         };
